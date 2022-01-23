c484efe4be68de51e0a65083f21288a687be09d7
DiegoAlvP <dialvarez.pro@gmail.com>
2022-01-23 01:48:57 -0500

    Chat finalizado v1

--- public/js/socket-chat.js
+++ public/js/socket-chat.js
@@ -1,51 +1,55 @@
 var socket = io();
 
-var params = new URLSearchParams( window.location.search );
+var params = new URLSearchParams(window.location.search);
 
-if ( !params.has( 'nombre' ) || !params.has( 'sala' ) ) {
+if (!params.has('nombre') || !params.has('sala')) {
   window.location = 'index.html';
-  throw new Error( 'El nombre y sala son necesarios' );
+  throw new Error('El nombre y sala son necesarios');
 }
 
 var usuario = {
-  nombre: params.get( 'nombre' ),
-  sala: params.get( 'sala' )
+  nombre: params.get('nombre'),
+  sala: params.get('sala')
 };
 
-// Conectarse al chat
 socket.on('connect', function() {
   console.log('Conectado al servidor');
 
-  socket.emit( 'entrarChat', usuario, function( resp ){
-    console.log('Usuarios conectados ', resp)
+  socket.emit('entrarChat', usuario, function( resp ) {
+    // console.log('Usuarios conectados', resp);
+    renderizarUsuarios( resp );
   });
+
 });
 
-// Escuchar
+// escuchar
 socket.on('disconnect', function() {
   console.log('Perdimos conexión con el servidor');
 });
 
 // Enviar información
-socket.emit('crearMensaje', {
-  usuario: 'Fernando',
-  mensaje: 'Hola Mundo'
-}, function(resp) {
-  console.log('respuesta server: ', resp);
-});
+// socket.emit('crearMensaje', {
+//     nombre: 'Fernando',
+//     mensaje: 'Hola Mundo'
+// }, function(resp) {
+//     console.log('respuesta server: ', resp);
+// });
 
 // Escuchar información
-socket.on('crearMensaje', function(mensaje) {
-  console.log('Servidor:', mensaje);
+socket.on('crearMensaje', function( mensaje ) {
+  // console.log('Servidor:', mensaje);
+  renderizarMensajes( mensaje, false );
+  scrollBottom();
 });
 
-// Escuchar cambiios de usuarios
+// Escuchar cambios de usuarios
 // cuando un usuario entra o sale del chat
-socket.on('listaPersona', function(persona) {
-  console.log(persona);
+socket.on('listaPersona', function( personas ) {
+  // console.log(personas);
+  renderizarUsuarios( personas );
 });
 
 // Mensajes privados
-socket.on( 'mensajePrivado', function( mensaje ) {
-  console.log('Mensaje privado: ', mensaje)
+socket.on('mensajePrivado', function( mensaje ) {
+  console.log('Mensaje Privado:', mensaje);
 });
